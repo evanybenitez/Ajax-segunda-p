217aa2fc8bb969dfe465437fa962efc0c3b1f8ea
Evany <evanygaleano@hotmail.com>
2016-11-03 16:51:43 -0300

    tarea segunda p

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	var panelTemplate;  //variable
+	$.get('template/panel.html',function(dom){
+		panelTemplate = dom;
+
+	});
+	$.ajax({//recupersr datos
+		type:'GET',
+		url:'api/cotizaciones',
+		success:function(data){
+			for(var i=0;i<data.length;i++){
+				var panel = $(panelTemplate);
+				var cotizacion = data[i];
+				panel.find('#moneda').append(cotizacion.moneda);
+				panel.find('#venta').append(cotizacion.venta);
+				panel.find('#compra').append(cotizacion.compra);
+				$('#principal').append(panel);
+			}
+		},
+		error:function(){
+			console.log('Algun error ha ocurrido');
+		}
+	});
+});
